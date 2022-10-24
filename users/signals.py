510f750b5a44eb9bf9d6f484f814b1d29af68fdb
Vladimir Bondarenko <higbond91@gmail.com>
2022-10-24 12:13:07 +0300

    first commit

--- users/signals.py
+++ users/signals.py
@@ -8,15 +8,13 @@ from django.core.exceptions import ObjectDoesNotExist
 def create_profile(sender, instance, created, **kwards):
     if created:
         Profile.objects.create(user=instance)
+    try:
+        instance.profile.save()
+    except ObjectDoesNotExist:
+        Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwards):
     instance.profile.save()
 
 
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    try:
-        instance.profile.save()
-    except ObjectDoesNotExist:
-        Profile.objects.create(user=instance)
